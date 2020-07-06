53f015e6745790cf99e738287c8bb0151d8991f4
Jomark Pangan <jomarkrazonpangan@gmail.com>
2020-07-06 16:53:15 +0800

    Added some informations

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,6 +13,8 @@ function About() {
           </a>
         </h3>
         <br />
+        <hr />
+        <br />
         <h1>What I Learned</h1>
         <ul className="learned-list">
           <li>Deploy React Apps on Github using gh-pages</li>
@@ -21,6 +23,23 @@ function About() {
           <li>Proper usage of forms</li>
           <li>
             <h4>Fundamentals of React</h4>
+          </li>
+        </ul>
+        <br />
+        <hr />
+        <br />
+        <h1>About Me</h1>
+        <h3>Name:  Jomark Pangan</h3>
+        <h3>Degree: BS Computer Engineering</h3>
+        <h3>Contacts:</h3>
+        <ul className="learned-list">
+          <li>Phone: (+63) 995 - 7340 - 183</li>
+          <li>Email: jomarkrazonpangan@gmail.com</li>
+          <li>
+            Github:{" "}
+            <a href="https://github.com/jomark031497">
+              https://github.com/jomark031497
+            </a>
           </li>
         </ul>
       </div>
